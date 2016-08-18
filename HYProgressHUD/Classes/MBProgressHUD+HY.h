bbf701890b406369f0b98bfbbb4d2909b26ee4b9
yclzone <yclzone@gmail.com>
2016-08-18 15:41:57 +0800

    HUD常用方法封装

--- HYProgressHUD/Classes/MBProgressHUD+HY.h
+++ HYProgressHUD/Classes/MBProgressHUD+HY.h
@@ -9,16 +9,39 @@
 #import <MBProgressHUD/MBProgressHUD.h>
 
 @interface MBProgressHUD (HY)
-+ (instancetype)showHUDAddedTo:(UIView *)view animated:(BOOL)animated text:(NSString *)text details:(NSString *)details;
-+ (instancetype)showText:(NSString *)text details:(NSString *)details;
++ (void)hy_showText:(NSString *)text
+            details:(NSString *)details
+      indeterminate:(BOOL)showIndeterminate
+             toView:(UIView *)view
+     hiddenanimated:(BOOL)animated
+              delay:(NSTimeInterval)delay
+    completionBlock:(MBProgressHUDCompletionBlock)completionBlock ;
 
-- (void)showTitle:(NSString *)title message:(NSString *)message hideAnimated:(BOOL)animated afterDelay:(NSTimeInterval)delay completionBlock:(MBProgressHUDCompletionBlock)completionBlock;
++ (void)hy_showText:(NSString *)text
+                    details:(NSString *)details
+           hiddenAfterDelay:(NSTimeInterval)delay
+            completionBlock:(MBProgressHUDCompletionBlock)completionBlock;
 
-- (void)showMessage:(NSString *)message
++ (void)hy_showText:(NSString *)text
+   hiddenAfterDelay:(NSTimeInterval)delay
     completionBlock:(MBProgressHUDCompletionBlock)completionBlock;
 
-+ (void)showMessage:(NSString *)message;
-+ (void)showMessage:(NSString *)message
-    completionBlock:(MBProgressHUDCompletionBlock)completionBlock;
-+ (void)showTitle:(NSString *)title message:(NSString *)message hideAnimated:(BOOL)animated afterDelay:(NSTimeInterval)delay completionBlock:(MBProgressHUDCompletionBlock)completionBlock;
++ (void)hy_showText:(NSString *)text;
+
+
+#pragma mark - 手动
++ (instancetype)hy_showText:(NSString *)text
+                    details:(NSString *)details
+              indeterminate:(BOOL)showIndeterminate
+                     toView:(UIView *)view
+                   animated:(BOOL)animated;
+
+- (void)hy_updateText:(NSString *)text
+           details:(NSString *)details
+      hideAnimated:(BOOL)animated
+        afterDelay:(NSTimeInterval)delay
+   completionBlock:(MBProgressHUDCompletionBlock)completionBlock;
+
+- (void)hy_updateText:(NSString *)text
+              details:(NSString *)details;
 @end
--- HYProgressHUD/Classes/MBProgressHUD+HY.m
+++ HYProgressHUD/Classes/MBProgressHUD+HY.m
@@ -9,59 +9,98 @@
 #import "MBProgressHUD+HY.h"
 
 @implementation MBProgressHUD (HY)
-+ (instancetype)showHUDAddedTo:(UIView *)view animated:(BOOL)animated text:(NSString *)text details:(NSString *)details {
-    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:animated];
-    hud.label.text = text;
-    hud.detailsLabel.text = details;
+
+#pragma mark - 自动消失
+
+
+/** 自动消失 */
++ (void)hy_showText:(NSString *)text
+            details:(NSString *)details
+      indeterminate:(BOOL)showIndeterminate
+             toView:(UIView *)view
+     hiddenanimated:(BOOL)animated
+              delay:(NSTimeInterval)delay
+    completionBlock:(MBProgressHUDCompletionBlock)completionBlock {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:YES];
+        hud.mode = (showIndeterminate ? MBProgressHUDModeIndeterminate : MBProgressHUDModeText);
+        hud.label.text = text;
+        hud.detailsLabel.text = details;
+        hud.completionBlock = completionBlock;
+        [hud hideAnimated:animated afterDelay:delay];
+    });
+}
+
+/** 纯文字 */
++ (void)hy_showText:(NSString *)text
+            details:(NSString *)details
+   hiddenAfterDelay:(NSTimeInterval)delay
+    completionBlock:(MBProgressHUDCompletionBlock)completionBlock {
+    
+    [self hy_showText:text
+                     details:details
+               indeterminate:NO
+                      toView:[UIApplication sharedApplication].keyWindow
+              hiddenanimated:YES
+                       delay:delay
+             completionBlock:completionBlock];
+}
+
++ (void)hy_showText:(NSString *)text
+   hiddenAfterDelay:(NSTimeInterval)delay
+    completionBlock:(MBProgressHUDCompletionBlock)completionBlock {
+    [self hy_showText:text details:nil hiddenAfterDelay:delay completionBlock:completionBlock];
+}
+
++ (void)hy_showText:(NSString *)text {
+    [self hy_showText:text details:nil hiddenAfterDelay:1 completionBlock:nil];
+}
+
+#pragma mark - 控制消失
++ (instancetype)hy_showText:(NSString *)text
+            details:(NSString *)details
+      indeterminate:(BOOL)showIndeterminate
+             toView:(UIView *)view
+           animated:(BOOL)animated {
+//    NSLog(@"%s -- %@", __FUNCTION__, [NSThread currentThread]);
+    
+    MBProgressHUD *hud = [[self alloc] initWithView:view];
+    hud.removeFromSuperViewOnHide = YES;
+    hud.mode = (showIndeterminate ? MBProgressHUDModeIndeterminate : MBProgressHUDModeText);
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        hud.label.text = text;
+        hud.detailsLabel.text = details;
+        [view addSubview:hud];
+        [hud showAnimated:animated];
+    });
+    
     return hud;
 }
 
-+ (instancetype)showText:(NSString *)text details:(NSString *)details {
-    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:[UIApplication sharedApplication].keyWindow animated:YES];
-    hud.label.text = text;
-    hud.detailsLabel.text = details;
-    return hud;
-}
-
-
-- (void)showTitle:(NSString *)title message:(NSString *)message hideAnimated:(BOOL)animated afterDelay:(NSTimeInterval)delay completionBlock:(MBProgressHUDCompletionBlock)completionBlock {
-    self.label.text = title;
-    self.detailsLabel.text = message;
+- (void)hy_updateText:(NSString *)text
+           details:(NSString *)details
+      hideAnimated:(BOOL)animated
+        afterDelay:(NSTimeInterval)delay
+   completionBlock:(MBProgressHUDCompletionBlock)completionBlock {
+//    NSLog(@"%s -- %@", __FUNCTION__, [NSThread currentThread]);
     self.completionBlock = completionBlock;
-    [self hideAnimated:animated afterDelay:delay];
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+        /** 必须在主线程，否则无效 */
+        self.label.text = text;
+        self.detailsLabel.text = details;
+        [self hideAnimated:animated afterDelay:delay];
+    });
 }
 
-- (void)showMessage:(NSString *)message afterDelay:(NSTimeInterval)delay
-    completionBlock:(MBProgressHUDCompletionBlock)completionBlock {
-    [self showTitle:nil message:message hideAnimated:YES afterDelay:delay completionBlock:completionBlock];
-}
-
-- (void)showMessage:(NSString *)message
-    completionBlock:(MBProgressHUDCompletionBlock)completionBlock {
-    [self showMessage:message afterDelay:1 completionBlock:completionBlock];
-}
-
-- (void)showMessage:(NSString *)message {
-    self.detailsLabel.text = message;
-}
-
-+ (void)showMessage:(NSString *)message
-    completionBlock:(MBProgressHUDCompletionBlock)completionBlock {
-    MBProgressHUD *hud = [self HUDForView:[UIApplication sharedApplication].keyWindow];
-    [hud showMessage:message completionBlock:completionBlock];
-}
-
-+ (void)showMessage:(NSString *)message {
-    MBProgressHUD *hud = [self HUDForView:[UIApplication sharedApplication].keyWindow];
-    [hud showMessage:message];
-}
-
-+ (void)showTitle:(NSString *)title message:(NSString *)message hideAnimated:(BOOL)animated afterDelay:(NSTimeInterval)delay completionBlock:(MBProgressHUDCompletionBlock)completionBlock {
-    MBProgressHUD *hud = [self HUDForView:[UIApplication sharedApplication].keyWindow];
-    hud.label.text = title;
-    hud.detailsLabel.text = message;
-    hud.completionBlock = completionBlock;
-    [hud hideAnimated:animated afterDelay:delay];
+- (void)hy_updateText:(NSString *)text
+              details:(NSString *)details {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        /** 必须在主线程，否则无效 */
+        self.label.text = text;
+        self.detailsLabel.text = details;
+    });
 }
 
 @end
